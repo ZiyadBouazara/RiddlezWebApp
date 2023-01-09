1a1cd83180a817690d708f70a164caa0ff161c9f
Ziyad Bouazara <ziyad.bouazara.1@ulaval.ca>
2023-01-09 15:09:17 -0800

    Add project files.

---
+++ Models/Riddle.cs
@@ -0,0 +1,14 @@
+﻿namespace RiddlezWebApp.Models
+{
+    public class Riddle
+    {
+        public int ID { get; set; }
+        public string RiddleQuestion { get; set; }
+        public string RiddleAnswer { get; set;}
+
+        public Riddle()
+        {
+
+        }
+    }
+}
